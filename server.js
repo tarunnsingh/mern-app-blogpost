f74b8175e38f185ebd3942be37d0be195807337f
thetseffect <thetseffect@gmail.com>
2020-05-16 17:00:10 +0530

    Added MongoDB

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-const morganMiddleware = require('./morganColor');
+const morganMiddleware = require("./morganColor");
+const { MONGODB_URI } = require("./credentials");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(morganMiddleware);
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose Connected!");
+});
 
 app.get("/api", (req, res) => [
   res.json({
