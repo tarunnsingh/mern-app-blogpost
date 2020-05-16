5d8fb053358e487bf662e331044e06744868c688
thetseffect <thetseffect@gmail.com>
2020-05-16 17:10:33 +0530

    Added Schema

--- server.js
+++ server.js
@@ -17,6 +17,35 @@ mongoose.connection.on("connected", () => {
   console.log("Mongoose Connected!");
 });
 
+const Schema = mongoose.Schema;
+
+const blogPostSchema = new Schema({
+  title: String,
+  body: String,
+  date: {
+    type: String,
+    default: Date.now(),
+  },
+});
+
+const BlogPost = mongoose.model("BlogPost", blogPostSchema);
+
+const data = {
+  title: 'Hola Friends!!',
+  body: 'Yo people how good you all look in this party of ours!'
+}
+const newBlogPost = new BlogPost(data)
+
+newBlogPost.save((error)=>{
+  if(error){
+    console.log(`Error occured in Saving: ${error}`)
+  } else {
+    console.log(`Data Saved successfully!`)
+  }
+})
+
+
+
 app.get("/api", (req, res) => [
   res.json({
     username: "tarun",
